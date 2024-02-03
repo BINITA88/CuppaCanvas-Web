ad39a6648e33efe3d96880d93a96d023cfdff34e
BINITA88 <122206547+BINITA88@users.noreply.github.com>
2024-02-03 10:06:59 +0545

    backendsetup

---
+++ CuppaCanvas-Backend/src/main/java/com/cuppacanvas/cuppacanvasbackend/CuppaCanvasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cuppacanvas.cuppacanvasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuppaCanvasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuppaCanvasBackendApplication.class, args);
+	}
+
+}
---
+++ CuppaCanvas-Backend/src/test/java/com/cuppacanvas/cuppacanvasbackend/CuppaCanvasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuppacanvas.cuppacanvasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuppaCanvasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
